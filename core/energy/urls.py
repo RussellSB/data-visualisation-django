9859333b248e8804e6175a705696429ef4b145e8
RussellSB <r.sammutbonnici@gmail.com>
2021-06-13 19:53:54 +0100

    Datatable visualisation with hyperlinks

--- core/energy/urls.py
+++ core/energy/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'energy'
 urlpatterns = [
-    path('building', views.BuildingView.as_view(), name='building')
+    path('building/', views.BuildingView.as_view(), name='building'),
+    path('building/<int:pk>/', views.BuildingSpecificView.as_view(), name='building_specific'),
+    path('meter/', views.MeterView.as_view(), name='meter'),
+    path('meter/<int:pk>/', views.MeterSpecificView.as_view(), name='meter_specific'),
+    path('halfhourly/', views.HalfHourlyView.as_view(), name='halfhourly')
 ]
--- core/energy/views.py
+++ core/energy/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Building
+from .models import Building, Halfhourly, Meter
+
+MAX = 200  # Max number of rows to display
 
 # Create your views here.
 class BuildingView(generic.ListView):
@@ -9,4 +11,27 @@ class BuildingView(generic.ListView):
     context_object_name = 'building_list'
 
     def get_queryset(self):
-        return Building.objects.all()
+        return Building.objects.all()[:MAX]
+
+class BuildingSpecificView(generic.DetailView):
+    model = Building
+    template_name = 'energy/building_specific.html'
+
+class MeterView(generic.ListView):
+    template_name = 'energy/meter.html'
+    context_object_name = 'meter_list'
+
+    def get_queryset(self):
+        return Meter.objects.all()[:MAX]
+
+class MeterSpecificView(generic.DetailView):
+    model = Meter
+    template_name = 'energy/meter_specific.html'
+
+    
+class HalfHourlyView(generic.ListView):
+    template_name = 'energy/halfhourly.html'
+    context_object_name = 'halfhourly_list'
+
+    def get_queryset(self):
+        return Halfhourly.objects.all()[:MAX]
