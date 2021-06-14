44473076f75bc89e0f27172c9b1cc708a53508c5
RussellSB <r.sammutbonnici@gmail.com>
2021-06-14 14:24:13 +0100

    Data visualisation

--- core/insight/views.py
+++ core/insight/views.py
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from energy.models import Building, Meter, Halfhourly
-from django.db.models import Sum, Q
+from django.db.models import Sum, Avg
 
 import time
    
@@ -16,9 +16,9 @@ class HomeView(View):
         return render(request, 'insight/index.html')
 
 
-def getTotalConsumption(unit_type):
+def getTotalConsumptionHotel(unit_type):
 
-    print('Computing total consumption ...')
+    print('Computing average consumption per hotel ...')
     start = time.time()
 
     # Making a dictionary for reference to hotel building names
@@ -40,15 +40,55 @@ def getTotalConsumption(unit_type):
         else: 
             labels.append(hotel)
 
-    chartdata = hotels.values()
+    data = hotels.values()
 
     print(time.time() - start, 'seconds for computing total consumption per hotel')
 
-    chartlegend = 'Total consumption per Hotel in 2018 ('+ unit_type +')'
-    return labels, chartdata, chartlegend
+    legend = 'Total Consumption per Hotel in 2018 ('+ unit_type +')'
+    return labels, data, legend
 
-hotel_cons_labels, hotel_cons_data, hotel_cons_legend = getTotalConsumption('kWh')  # Making it a constant so it doesnt need to be computed every time a request is sent
-   
+
+def getAvgConsumptionTime(unit_type):
+
+    print('Computing average consumption per hour ...')
+    start = time.time()
+
+    # Making a dictionary for reference to hours
+    options_hours = [x for x in range(0, 24)]
+    options_minutes = [0, 30]
+
+    labels = []
+    data = []
+
+    for h in options_hours:
+        for m in options_minutes:
+            # Compute consumption average per hour, minute combination
+            out = Halfhourly.objects.filter(meter_id__unit__contains=unit_type)\
+                                    .filter(reading_date_time__hour__in=[h])\
+                                    .filter(reading_date_time__minute__in=[m])\
+                                    .aggregate(Avg('consumption'))
+            out = float(out['consumption__avg'])
+            data.append(out)
+
+            # Formatting corresponding time labels
+            t = [h, m]
+            for i in range(len(t)):
+                t[i] = str(t[i])
+                if len(t[i]) == 1: t[i] = '0'+t[i]
+            t = t[0] + ':' + t[1]
+            labels.append(t)
+
+    print(time.time() - start, 'seconds for computing average consumption per hour')
+
+    legend = 'Average Consumption per Half Hour End in 2018 ('+ unit_type +')'
+    return labels, data, legend 
+
+
+# Making it a global scope so it is only computed once
+hotel_cons_labels, hotel_cons_data, hotel_cons_legend = getTotalConsumptionHotel('kWh')  
+time_cons_labels, time_cons_data, time_cons_legend = getAvgConsumptionTime('kWh') 
+
+
 ## using rest_framework classes
 class ChartData(APIView):
     authentication_classes = []
@@ -60,6 +100,9 @@ class ChartData(APIView):
         body ={
             'hotel_cons_legend': hotel_cons_legend,
             'hotel_cons_labels': hotel_cons_labels,
-            'hotel_cons_data': hotel_cons_data
+            'hotel_cons_data': hotel_cons_data,
+            'time_cons_legend': time_cons_legend,
+            'time_cons_labels': time_cons_labels,
+            'time_cons_data': time_cons_data,
         }
         return Response(body)
